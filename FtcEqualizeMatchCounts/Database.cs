30f9c82173d31f2641b4c0ebace5bee912a2d79d
Robert Atkinson <bob@theatkinsons.org>
2019-12-16 10:59:19 -0800

    remove System.Data.Sqlite

--- FtcEqualizeMatchCounts/Database.cs
+++ FtcEqualizeMatchCounts/Database.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SQLite;
 using System.IO;
-using System.Linq;
-using System.Net.Configuration;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FtcEqualizeMatchCounts
     {
